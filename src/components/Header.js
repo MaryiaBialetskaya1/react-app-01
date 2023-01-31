f0a1c03e0094b5d8afc50b68d3e33d1edf6496d6
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-31 17:38:48 -0500

    start work on Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Organaze Maryia's Birthday</h1>
+        </div>
+        <div>
+          <h2>Type: personal</h2>
+          <h2>Due Fri 11 Aug 2023</h2>
+        </div>
+      </div>
+    );
+  }
+}
