1ae2a837a9e1b963023402a36187c0b3ba4e2bc7
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-01 16:51:15 -0500

    add component List

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export class List extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is List Section</h1>
+      </div>
+    );
+  }
+}
