1721daf8f70bc462a0485e44d6d867064ff5bf26
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-02 15:21:27 -0500

    add data to List Component

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,23 @@
 import { Component } from "react";
 
+let listItem = [
+  { id: 1, task: "Send invitations" },
+  { id: 2, task: "Book the clown" },
+  { id: 3, task: "Order cupkes" },
+];
+
 export class List extends Component {
   render() {
     return (
       <div>
-        <h1>This is List Section</h1>
+        <h1>Create new task</h1>
+        <div>
+          <textarea></textarea>
+        </div>
+        <div>
+          <button>Add</button>
+        </div>
+        <div>list elements</div>
       </div>
     );
   }
