e0670098bd06a912dbf49e3d510f7a4a5e26018c
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-02 14:39:03 -0500

    add info to state

--- src/state/state.js
+++ src/state/state.js
@@ -1,2 +1,13 @@
-let store = {};
+let store = {
+  _state: {
+    listPage: {
+      tasks: [
+        { id: 1, task: "Send invitations" },
+        { id: 2, task: "Book the clown" },
+        { id: 3, task: "Order cupkes" },
+      ],
+      newTaskText: "Add new task...",
+    },
+  },
+};
 export default store;
