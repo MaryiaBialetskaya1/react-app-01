2e564aa20e8f39ee55fbb7b3fbe99bdf483252c4
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-02 21:08:30 -0500

    add onChange event

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,18 +1,26 @@
 import { Component } from "react";
 
-let ListItem = [
-  { id: 1, task: "Send invitations" },
-  { id: 2, task: "Book the clown" },
-  { id: 3, task: "Order cupkes" },
-];
-
 export class List extends Component {
+  state = {
+    input: "",
+    listItems: [],
+  };
+  onChangeEvent(e) {
+    this.setState({ input: e });
+    console.log(e);
+  }
   render() {
     return (
       <div>
         <h1>Create new task</h1>
         <div>
-          <textarea></textarea>
+          <textarea
+            placeholder="add task"
+            onChange={(e) => {
+              this.onChangeEvent(e.target.value);
+            }}
+            value={this.state.input}
+          ></textarea>
         </div>
         <div>
           <button>Add</button>
