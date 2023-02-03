7b5ee43b8c6a92138de3c7c7f6c74be95fec1712
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-03 13:46:44 -0500

    edit errors

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,34 +5,55 @@ export class List extends Component {
     input: "",
     listItems: [],
   };
+  onFormSubmit(e) {
+    e.preventDefault();
+  }
   onChangeEvent(e) {
     this.setState({ input: e });
     console.log(e);
   }
   addItem(input) {
-    let items = this.state.listItems;
-    items.push(input);
-    this.setState({ listItems: items, input: "" });
-    console.log(items);
+    if (input === "") {
+      alert("Enter an item. No empty strings.");
+    } else {
+      let items = this.state.listItems;
+      items.push(input);
+      this.setState({ listItems: items, input: "" });
+      console.log(items);
+    }
+  }
+
+  markedWord(e) {
+    const item = e.target;
+    item.classList.toggle("marked");
   }
   render() {
     return (
       <div>
-        <h1>Create new task</h1>
-        <div>
-          <textarea
-            placeholder="add task"
-            onChange={(e) => {
-              this.onChangeEvent(e.target.value);
-            }}
-            value={this.state.input}
-          ></textarea>
-        </div>
-        <div>
-          <button onClick={() => this.addItem(this.state.listItems)}>
-            Add
-          </button>
-        </div>
+        <form onSubmit={this.onFormSubmit}>
+          <h1>Create new task</h1>
+          <div>
+            <textarea
+              placeholder="add task"
+              onChange={(e) => {
+                this.onChangeEvent(e.target.value);
+              }}
+              value={this.state.input}
+            ></textarea>
+          </div>
+          <div>
+            <button onClick={() => this.addItem(this.state.input)}>Add</button>
+          </div>
+          <div>
+            <ul>
+              {this.state.listItems.map((item, index) => (
+                <li onClick={this.markedWord} key={index}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </form>
       </div>
     );
   }
